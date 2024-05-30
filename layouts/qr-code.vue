deff572d9e3d9e61a9122d0ed85c16574733fc49
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2024-05-30 17:26:40 -0500

    Update with custom QR code

--- layouts/qr-code.vue
+++ layouts/qr-code.vue
@@ -1,22 +1,51 @@
 <script setup lang="ts">
-import { computed } from "vue";
-import QRious  from "qrious";
+import { computed, onMounted } from "vue";
+import QRCodeStyling from "qr-code-styling";
 
 const url = window.location.href.replace(/\/[^\/]*$/, "/");
 
 const qrcode = computed(() => {
-  return new QRious({
-    value: url,
-    size: 300,
-  }).toDataURL();
+  return new QRCodeStyling({
+    width: 300,
+    height: 300,
+    type: "svg",
+    data: url,
+    image: "luke.jpeg",
+    dotsOptions: {
+      color: "#000000",
+      type: "classy-rounded",
+    },
+    backgroundOptions: {
+      color: "#ffffff",
+    },
+    imageOptions: {
+      margin: 10,
+      imageSize: 0.5,
+    },
+  })
+});
+
+onMounted(() => {
+  document.getElementById("qrContainer").innerHTML = "";
+  qrcode.value.append(document.getElementById("qrContainer"));
 });
 </script>
 
 <template>
   <div class="slidev-layout qrcode text-center">
     <slot />
-    <div class="container mx-auto mt-8">
-      <img :src="qrcode" alt="QR Code" class="inline-block" />
+    <div class="container mx-auto mt-8 flex">
+      <div id="qrContainer" class="mx-auto"></div>
     </div>
   </div>
 </template>
+
+<style>
+#qrContainer > svg {
+  @apply rounded-lg;
+}
+
+#qrContainer > svg > image {
+  @apply rounded-lg;
+}
+</style>
